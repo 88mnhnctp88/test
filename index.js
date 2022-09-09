78cfec6fd64c72f1511e04f53f374adb582c88bc
88mnhnctp88 <113150048+88mnhnctp88@users.noreply.github.com>
2022-09-08 17:25:30 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('messageCreate', message => {
+  if (message.content.toLowerCase() == '%ping') {
+    message.channel.send('pong');
+  }
+
+
+});
+
+client.login(`process.env.token`);
